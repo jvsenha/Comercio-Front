a3e33583d0f96f1851c8eac051479815f38bf022
João Vitor Senha Dos Santos <joao.santos403@fatec.sp.gov.br>
2025-09-16 19:57:43 -0300

    initial commit

---
+++ src/app/sexo/sexo.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sexo',
+  imports: [],
+  templateUrl: './sexo.html',
+  styleUrl: './sexo.scss'
+})
+export class Sexo {
+
+}
